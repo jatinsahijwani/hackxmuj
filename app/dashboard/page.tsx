52908ba3a56171922079a9caa7a1377023a4ea3c
Anirudh Singh Chouhan <140189477+AnirudhSingh07@users.noreply.github.com>
2024-09-05 16:47:50 +0530

    dashboard

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Dashboard } from '@/components/component/dashboard'
+const page = () => {
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  )
+}
+
+export default page
